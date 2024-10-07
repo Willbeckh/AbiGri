214db9990078fd6be146eb49fd60cc6726b7e248
Willbeckh <wileyhndirangu04#gmail.com>
2024-10-07 18:00:56 +0300

    update app configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -9,11 +10,14 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        button: "#38af49",
+        "primary-background": "#031306",
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
 };
 export default config;
