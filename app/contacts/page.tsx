88ae10ec4f08cb8ab6b76e31d25f6ff00af11b03
Willbeckh <wileyhndirangu04#gmail.com>
2024-10-08 19:20:09 +0300

    create contacts pages

---
+++ app/contacts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Contacts Pgae</h1>
+      <p>Reach out us!</p>
+    </div>
+  );
+};
+
+export default Contacts;
