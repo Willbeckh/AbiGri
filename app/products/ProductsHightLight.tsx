8a3bb29f7c136828ac27f9854f754c6e23a31351
Willbeckh <wileyhndirangu04#gmail.com>
2024-10-08 19:22:55 +0300

    ft: create featured products section

--- app/products/ProductsHightLight.tsx
+++ app/products/ProductsHightLight.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { supabase } from "@/utils/supabaseClient";
+import Image from "next/image";
 
 const ProductHighlights = async () => {
-  const { data: featuredProducts } = await supabase
+  const { data: featuredProducts, error } = await supabase
     .from("products")
-    .select("*, product_categories(*)");
+    .select("*, product_categories(*)")
+    .eq("featured", true);
+
+  if (error) {
+    return <p>Failed to fetch data</p>;
+  }
+
+  if (!featuredProducts) {
+    return (
+      <div className="loading loading-dots">
+        <p>Loading products data</p>
+      </div>
+    );
+  }
 
   console.log("featuredProducts: ", featuredProducts);
 
@@ -13,14 +27,26 @@ const ProductHighlights = async () => {
       <h1 className=" text-green-500  text-3xl lg:mt-20 mt-12 mb-4 lg:ps-6 border-b-2 border-green-400 pb-4">
         Featured Products
       </h1>
-      <div className="flex flex-col lg:flex-row flex-1 justify-between p-8">
-
-        <div className="">
-          <pre>{JSON.stringify(featuredProducts, null)}</pre>
-        </div>
-        <div className=""> Product 2</div>
-        <div className=""> Product 3</div>
-        <div className=""> Product 4</div>
+      <div className="flex flex-col lg:flex-row flex-1 columns-3  p-8">
+        {featuredProducts &&
+          featuredProducts.map((product, index) => (
+            <div key={index} className="flex-auto">
+              <div className="card glass lg:w-4/5 mb-4 h-80">
+                <figure>
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    width={400}
+                    height={400}
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{product.title}</h2>
+                  <p className="text-sm">{product.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
