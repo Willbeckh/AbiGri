853554da7529da26454841f2abb54cf313e801ee
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-20 05:04:00 -0700

    create dynamic header component

---
+++ components/auth/header.tsx
@@ -0,0 +1,26 @@
+import { Archivo } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Archivo({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+interface HeaderProps {
+  label: string;
+}
+
+export const Header = ({ label }: HeaderProps) => {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <h1
+        className={cn("text-3xl font-semibold text-gray-900 ", font.className)}
+      >
+        Auth
+      </h1>
+      <div className="w-1/2 h-1 bg-green-500 rounded-full mb-2"></div>
+
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  );
+};
