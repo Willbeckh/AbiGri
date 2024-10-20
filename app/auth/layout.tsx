9c12d06eb5104b18f0a5b0316993c4a576e3dd3f
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-20 06:22:15 -0700

    fix: change div height and padding

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen  flex items-center justify-center bg-green-50">
+    <div className="h-full p-8 flex items-center justify-center bg-green-50">
       {children}
     </div>
   );
