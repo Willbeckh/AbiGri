27d01e8937438287fb518b6c88a45d13b51e2f2d
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-20 06:22:59 -0700

    create register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,9 @@ export const LoginSchema = z.object({
   email: z.string().email("Email is required."),
   password: z.string().min(4, "Password is required."),
 });
+
+export const RegisterSchema = z.object({
+  name: z.string().min(3, "Name is required."),
+  email: z.string().email("Email is required."),
+  password: z.string().min(8, "Minimum 8 characters required!"),
+});
