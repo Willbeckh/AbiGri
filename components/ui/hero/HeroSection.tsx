e616ca21427ec5656fc6e31f0e5639e19cb0fcf5
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-21 22:59:47 -0700

    st: add partners ui component

--- components/ui/hero/HeroSection.tsx
+++ components/ui/hero/HeroSection.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import HeroContent from "./hero-content";
 import ImageSlider from "./image-slider";
+import { Partners } from "./partners";
 
 interface ImageSliderProps {
   images: {
@@ -13,12 +14,16 @@ interface ImageSliderProps {
 const HeroSection: React.FC<ImageSliderProps> = ({ images }) => {
   return (
     <>
-      <div className="w-11/12 max-w-7xl overflow-hidden grid md:grid-cols-2 gap-y-2 gap-x-4">
+      <div className="w-11/12 max-w-7xl overflow-hidden grid mt-10 md:grid-cols-2 gap-y-2 gap-x-4">
         <div className="cols-span-1 p-8 ">
           <HeroContent />
         </div>
         <div className="py-6 h-fit my-auto">
           <ImageSlider images={images} />
+        </div>
+
+        <div className="flex justify-center md:col-span-3 p-2 mt-6 mb-2">
+          <Partners />
         </div>
       </div>
     </>
