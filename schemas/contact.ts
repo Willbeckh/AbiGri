8ab1bc752f40f49f8dbbce6f6fe7be2edf6d7beb
Willbeckh <wileyhndirangu01@gmail.com>
2024-10-23 21:03:42 -0700

    add contact validation schema

---
+++ schemas/contact.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const ContactSchema = z.object({
+  name: z.string().min(3, "Name is required."),
+  email: z.string().email("Email is required."),
+  phone: z.string().min(10, "Phone is required."),
+  subject: z.string().min(3, "Subject is required."),
+  message: z.string().min(10, "Message is required."),
+});
