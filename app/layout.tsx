6eee6ed4ca0374b5472f428030dc47015d5fb46c
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-25 11:45:49 -0700

    remove default font settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/ui/NavBar";
 import Footer from "./footer/page";
-import { menuItems } from "./data/menuData";
+import { menuItems } from "../data/menuData";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "AbiGri",
@@ -30,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressContentEditableWarning={true}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="">
         <Analytics />
         <SpeedInsights />
         <NavBar menuItems={menuItems} />
