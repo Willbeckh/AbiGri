6b472a68a7fb57e2097282b67d0c96d2207964b9
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-25 12:56:16 -0700

    add logic to create new user

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server";
-import * as z from "zod";
+import z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -11,5 +13,30 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     };
   }
 
-  return { success: "Email sent" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      error: "Email already exists!",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return {
+    success: "Sign Up successful! Proceed to login.",
+  };
 };
