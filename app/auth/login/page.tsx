9c59a25661d38a270557eac4c8894a11198ec765
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-27 16:16:52 +0300

    add redirect logic on login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import { LoginForm } from "@/components/auth/login-form";
 
-const LoginPage = () => {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
+const LoginPage = async () => {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+
+  if (data.user) {
+    redirect("/dashboard");
+  }
+  return <LoginForm />;
 };
 
 export default LoginPage;
