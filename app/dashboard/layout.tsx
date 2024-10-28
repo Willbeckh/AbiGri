37ad549d4e58be20af89b59dfedbdec7297a6664
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-28 15:52:53 +0300

    update user dashboard style

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen p-8 flex  justify-center mt-20 bg-green-50">
-      {children}
+    <div className="min-h-screen p-8 flex bg-green-50">
+      <div className="mt-20">{children}</div>
     </div>
   );
 };
