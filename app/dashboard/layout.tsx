0144dfc68843fdc3a13846e9bc739d7bf663da08
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-31 21:01:36 +0300

    Add user dashboard root layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { DashboardNav } from "@/components/dashboard/nav";
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen p-8 flex bg-green-50">
-      <div className="mt-20">{children}</div>
-    </div>
+    <SidebarProvider>
+      <DashboardNav />
+      <main className="bg-green-50 w-full px-14">
+        <div className="pt-24">
+          <div className="flex">
+            <SidebarTrigger tabIndex={0} />
+            <h1 className="font-bold">Dashboard</h1>
+          </div>
+          <div className="">{children}</div>
+        </div>
+      </main>
+    </SidebarProvider>
   );
 };
 
