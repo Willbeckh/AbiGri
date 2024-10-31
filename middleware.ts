0085d0a7d7984ba23b20a79c5da2cf9e52b20692
Willbeckh <wileyhndirangu04@gmail.com>
2024-10-31 21:02:00 +0300

    ft: update middleware to protect all dashboard routes

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard", "/products"],
-  // matcher: [
-  //   
-  //   '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  // ],
+  matcher: ["/dashboard/:path*", "/products"],
 };
