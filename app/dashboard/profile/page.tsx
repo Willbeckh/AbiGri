abc83fc3469c72968736a6a17d8b095d6b0730d2
Willbeckh <wileyhndirangu04@gmail.com>
2024-11-04 20:06:35 +0300

    feat: add auth null checker + add profile prop to [UserProfile] component

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -3,10 +3,17 @@ import { Separator } from "@/components/ui/separator";
 import { UserProfile } from "@/components/dashboard/user-profile";
 import { UserTools } from "@/components/dashboard/user-tools";
 import { createClient } from "@/utils/supabase/server";
+import { getProfile } from "@/utils/supabase/profile";
 
 export default async function ProfilePage() {
   const supabase = await createClient();
   const { data } = await supabase.auth.getUser();
+
+  if (!data.user) {
+    return <div>User not authenticated.</div>;
+  }
+
+  const profileData = await getProfile(data.user.id);
 
   return (
     <div className="flex-1 md:space-y-4 py-2 md:p-8 ">
@@ -14,7 +21,9 @@ export default async function ProfilePage() {
         <Card className="p-6">
           <h3 className="text-xl font-semibold">User Information</h3>
           <Separator className="my-4" />
-          {data.user && <UserProfile user={data.user} />}
+          {data.user && profileData && (
+            <UserProfile user={data.user} profile={profileData[0]} />
+          )}
         </Card>
         <Card className="p-6">
           <h3 className="text-xl font-semibold">Tools & Equipment</h3>
