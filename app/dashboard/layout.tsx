482b21497d4ac223d9ff927cced1fd81c881672d
Willbeckh <wileyhndirangu04@gmail.com>
2024-11-04 21:23:26 +0300

    style: add tooltip to sidebar icon

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,12 +4,17 @@ import { DashboardNav } from "@/components/dashboard/nav";
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={false}>
       <DashboardNav />
-      <main className="bg-green-50 w-full px-14">
+      <main className="bg-green-100 w-full px-14">
         <div className="pt-24">
           <div className="flex">
-            <SidebarTrigger tabIndex={0} />
+            <div
+              className="tooltip tooltip-info tooltip-bottom"
+              data-tip="Open sidebar"
+            >
+              <SidebarTrigger tabIndex={0} size="icon" />
+            </div>
             <h1 className="font-bold">Dashboard</h1>
           </div>
           <div className="">{children}</div>
